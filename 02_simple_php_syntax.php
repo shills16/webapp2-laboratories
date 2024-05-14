3a5e14d123ef14ddf21a53929bfff7cc701280ff
shie <166887789+shills16@users.noreply.github.com>
2024-05-14 06:28:24 -0700

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,70 @@
+<?php 
+
+// TASK 1
+$name = 'Shiela Mae T. Miclat' ; 
+$age = 20 ;
+$address = 'Chapelside Batasan, Macabebe, Pampanga' ;
+$hobbies = 'Watching Kdrama/Movie' ;
+$pet_peeve = 'Talking loudly on the phone in public' ;
+
+echo 'TASK 1', '<br><br>' ;
+echo 'Name: ' . $name, '<br>' ;
+echo 'Age: ' . $age, '<br>' ;
+echo 'Address: ' . $address, '<br>' ;
+echo 'Hobbies: ' . $hobbies, '<br>' ;
+echo 'Pet_peeve: ' . $pet_peeve, '<br>' ;
+
+echo '<br><br><br>' ;
+
+
+// TASK 2
+$name = 'Shiela Mae T. Miclat' ; 
+$age = 20 ;
+$address = 'Chapelside Batasan, Macabebe, Pampanga' ;
+// $hobbies = 'Watching Kdrama/Movie' ;
+// $pet_peeve = 'Talking loudly on the phone in public' ;
+
+echo 'TASK 2', '<br><br>' ; 
+echo 'Name: ' . $name, '<br>' ;
+echo 'Age: ' . $age, '<br>' ;
+echo 'Address: ' . $address, '<br>' ;
+echo 'Hobbies: ' . $hobbies, '<br>' ;
+echo 'Pet_peeve: ' . $pet_peeve, '<br>' ;
+
+echo '<br><br><br>' ;
+
+// TASK 3
+define('NAME', 'Shiela Mae T. Miclat') ; 
+define('AGE', 20) ;
+define('ADDRESS', 'Chapelside Batasan, Macabebe, Pampanga') ;
+define('HOBBIES', 'Watching Kdrama/Movie') ;
+define('PET_PEEVE', 'Talking loudly on the phone in public') ;
+
+echo 'TASK 3', '<br><br>' ;
+echo 'Name: ' . NAME, '<br>';
+echo 'Age: ' . AGE, '<br>' ;
+echo 'Address' . ADDRESS, '<br>' ;
+echo 'Hobbies: ' . HOBBIES, '<br>' ;
+echo 'Pet_peeve: ' . PET_PEEVE, '<br>' ;
+
+echo '<br><br><br>' ;
+
+
+// TASK 4
+echo 'TASK 4', '<br>' ;
+echo '<pre>', '<br/>' ;
+var_dump(NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE) ;
+
+// TASK 5
+
+/*
+    Talking loudly on the phone in public is one of my 
+    pet peeve because it disrupts the peace and quiet 
+    of others. It shows a lack of consideration for those 
+    around you and can be seen as rude and inconsiderate 
+    behavior. It is important to be mindful of your surroundings 
+    and keep noise levels to a minimum when using your 
+    phone in public spaces.
+*/
+
+
