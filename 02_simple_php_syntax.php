f81b674431845d892f83e5283443910ab8911a57
shie <166887789+shills16@users.noreply.github.com>
2024-05-15 02:08:54 -0700

    Update 02_simple_php_syntax.php

--- 02_simple_php_syntax.php
+++ 02_simple_php_syntax.php
@@ -7,14 +7,11 @@ $address = 'Chapelside Batasan, Macabebe, Pampanga' ;
 $hobbies = 'Watching Kdrama/Movie' ;
 $pet_peeve = 'Talking loudly on the phone in public' ;
 
-echo 'TASK 1', '<br><br>' ;
 echo 'Name: ' . $name, '<br>' ;
 echo 'Age: ' . $age, '<br>' ;
 echo 'Address: ' . $address, '<br>' ;
 echo 'Hobbies: ' . $hobbies, '<br>' ;
 echo 'Pet_peeve: ' . $pet_peeve, '<br>' ;
-
-echo '<br><br><br>' ;
 
 
 // TASK 2
@@ -23,13 +20,6 @@ $age = 20 ;
 $address = 'Chapelside Batasan, Macabebe, Pampanga' ;
 // $hobbies = 'Watching Kdrama/Movie' ;
 // $pet_peeve = 'Talking loudly on the phone in public' ;
-
-echo 'TASK 2', '<br><br>' ; 
-echo 'Name: ' . $name, '<br>' ;
-echo 'Age: ' . $age, '<br>' ;
-echo 'Address: ' . $address, '<br>' ;
-echo 'Hobbies: ' . $hobbies, '<br>' ;
-echo 'Pet_peeve: ' . $pet_peeve, '<br>' ;
 
 echo '<br><br><br>' ;
 
@@ -40,7 +30,6 @@ define('ADDRESS', 'Chapelside Batasan, Macabebe, Pampanga') ;
 define('HOBBIES', 'Watching Kdrama/Movie') ;
 define('PET_PEEVE', 'Talking loudly on the phone in public') ;
 
-echo 'TASK 3', '<br><br>' ;
 echo 'Name: ' . NAME, '<br>';
 echo 'Age: ' . AGE, '<br>' ;
 echo 'Address' . ADDRESS, '<br>' ;
@@ -51,12 +40,10 @@ echo '<br><br><br>' ;
 
 
 // TASK 4
-echo 'TASK 4', '<br>' ;
-echo '<pre>', '<br/>' ;
+echo '<pre>' ;
 var_dump(NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE) ;
 
 // TASK 5
-
 /*
     Talking loudly on the phone in public is one of my 
     pet peeve because it disrupts the peace and quiet 
