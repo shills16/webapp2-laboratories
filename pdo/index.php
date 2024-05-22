4ac0f6bc28a6f5a404064d31cea1214addab07ab
shills16 <shielamiclat9@gmail.com>
2024-05-22 16:12:44 -0700

    Task 3 finished

---
+++ pdo/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'config.php';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+
+    if ($pdo) {
+        echo "Connected to the $db database successfully!";
+
+        // execute a query
+        // $sql = 'SELECT * FROM customers';
+        // $statement = $pdo->query($sql);
+
+        // $customers = $statement->FetchAll(PDO::FETCH_ASSOC);
+
+        // echo '<pre>';
+        // print_r($customers);
+        // echo '</pre>';
+    }
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
+
