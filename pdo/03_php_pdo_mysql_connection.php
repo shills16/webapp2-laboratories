be3d7fbd5fa7d8f2ff37bfda2787254bd822d2bc
shie <166887789+shills16@users.noreply.github.com>
2024-05-25 13:00:33 +0800

    Update 03_php_pdo_mysql_connection.php

--- pdo/03_php_pdo_mysql_connection.php
+++ pdo/03_php_pdo_mysql_connection.php
@@ -5,4 +5,21 @@ $db = 'students';
 $user = 'root';
 $password = '';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+
+    if ($pdo) {
+        echo "Connected to the $db database successfully!";
+
+        // execute a query
+    }
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
 ?>
